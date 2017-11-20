95f79da465ae2e0034df61e4292efb731a214b13
Paula Barszcz <paula.anna.barszcz@gmail.com>
2017-11-20 15:42:37 +0100

    Mock-ups: main-nav is done.

--- mock-ups/app.js
+++ mock-ups/app.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    console.log("podpiety");
+     //wlacza wylacza mobilna nawigacje
+    $('.main-nav-toogle').on('click', function() {
+            $('body').toggleClass('nav-show');
+        });
 
+    //po kliknieciu na link w menu przewijam strone
+    $('.main-nav a').on('click', function(e) {
+        e.preventDefault();
+        const href = $(this).attr('href');
+        const $target = $(href);
 
+        $('body, html').animate({
+            scrollTop : $target.offset().top
+        }, 1500);
+    })
+      
 });
