5cb3d0be9ca9ed1436a6a799ae92264934e082a1
Paula Barszcz <paula.anna.barszcz@gmail.com>
2017-11-21 14:03:21 +0100

    Navigation is done.

--- src/js/components/navigation.jsx
+++ src/js/components/navigation.jsx
@@ -3,30 +3,43 @@ import {
     Link
 } from 'react-router';
 
+
+
 class Navigation extends React.Component {
     constructor(props){
         super(props)
     }
 
-    render() {
-        return  <div>
-            <ul className="navigation">
-            <li>
-                <Link to="/home">HOME</Link>
-            </li>
-            <li>
-                <Link to="/slider">SLIDER</Link>
-            </li>
-            <li>
-                <Link to="/quiz">QUIZ</Link>
-            </li>
-            <li>
-                <Link to="/infotable">INFOTABLE</Link>
-            </li>
-            </ul>
+    handleNavClick = (e) => {
+        console.log("kliknieto: e.target :",e.target);
+        console.log("kliknieto: e :",e);
+        let body = document.querySelector("body");
+        console.log('body',body);
+        body.classList.toggle("nav-show");
 
+
+    }
+    render() {
+
+        return <div className="divNav">
+            <button className="main-nav-toogle" onClick={e => this.handleNavClick(e)}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <strong>Pokaż menu</strong>
+            </button>
+            <nav className="main-nav">
+                <ul>
+                    <li><Link to="/home">HOME</Link></li>
+                    <li><Link to="/slider">SLIDER</Link></li>
+                    <li><Link to="/quiz">QUIZ</Link></li>
+                    <li><Link to="/infotable">INFOTABLE</Link></li>
+                </ul>
+            </nav>
             {this.props.children}
+            
         </div>
+
     }
 }
 
