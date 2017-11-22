9c3631265491b40b80b04c9ac9acb44d0d50c252
Paula Barszcz <paula.anna.barszcz@gmail.com>
2017-11-22 17:28:45 +0100

    Slider in progress

--- src/js/components/slider.jsx
+++ src/js/components/slider.jsx
@@ -7,7 +7,13 @@ class Slider extends React.Component{
         this.state = {  
             indexSlider: 0,
             objList: [],
-            objLength: 0  
+            objLength: 0,
+            name: '',
+            surname: '',
+            nationality: '',
+            imdb: '',
+            photo: '',
+            currentId: 0
         }
     }
 
@@ -17,13 +23,39 @@ class Slider extends React.Component{
 
             this.objList.push(response);
             this.objLength = response.length;
-            console.log('ooo',this.objLength);
+
+            //console.log(this.state.objList[0][4].imdb);
+            let namesSurnames = [];
+            for (let i=0; i<response.length; i++){
+                let name = response[i].name;
+                let surname = response[i].surname;
+                let both = `${name} ${surname}`;
+                namesSurnames.push(both); 
+            }
+
+            //console.log('namesSurnames',namesSurnames);
+
+            //console.log('xx this.state.objLength',this.state.objLength);
+
+            console.log("response.length",response.length);
+            let currentId = Math.ceil(Math.random()*response.length);
+            this.name = response[currentId].name;
+            this.surname = response[currentId].surname;
+            this.nationality = response[currentId].nationality;
+            this.imdb = response[currentId].imdb;
+            this.photo = response[currentId].photo;
 
             this.setState({
                 objList: this.objList,
-                objLength: this.objLength
+                objLength: this.objLength,
+                name: this.name,
+                surname: this.surname,
+                nationality: this.nationality,
+                imdb: this.imdb,
+                photo: this.photo,
+                currentId: currentId
             })
-            console.log(this.state.objList[0][4].imdb);
+
         });
     }
 
@@ -35,43 +67,43 @@ class Slider extends React.Component{
         })
         console.log("prev, this.state.indexSlider",this.state.indexSlider);
 
+        console.log('this.state.name z klik prev',this.state.name);
+        console.log('this.state.objList z klik prev',this.state.objList);
+
     }
+
+
 
     handleClickNext = () => {
         this.setState({
-            indexSlider: this.state.indexSlider +1
+            currentId: this.state.currentId +1
         })
-        console.log("next, this.state.indexSlider",this.state.indexSlider);
+
+        
+        //console.log("next, this.state.indexSlider",this.state.indexSlider);
+        //console.log('this.state.name z klik next',this.state.name);
+        //console.log('this.state.objList z klik next',this.state.objList);
 
     }
 
-
     render(){
-        let copyOfObj = this.state.objList.slice();
-        //let objects = copyOfObj[0];
-        //console.log('objects',objects);
-        console.log(this.state.objList);
-        console.log('copyOfObj',copyOfObj.length);
-        console.log('oioi',this.state.objLength);
-
-        let namesSurnames = [];
-
-        for (let i=1; i<this.state.objLength; i++){
-            //console.log('copyOfObj[i]',copyOfObj[0][i].surname);
-            let name = copyOfObj[0][i].name;
-            let surname = copyOfObj[0][i].surname;
-            let both = name+' '+surname;
-            //console.log(both);
-            namesSurnames.push(both);
+        if (this.state.objLength !==0){
+            console.log("!@! z render this.state.currentId",this.state.currentId);
+          
+            //console.log("niepusty!");
+            //console.log('z rendera! this.state....',this.state.name, this.state.surname, this.state.nationality, this.state.imdb);
+            let incrementedId = this.state.currentId+1;
+            console.log('incrementedId',incrementedId);
+            let copyOfObj = this.state.objList.slice();
+            console.log('copyOfObj Z RENDERA1',copyOfObj[0][this.state.currentId].photo);
+            console.log('copyOfObj Z RENDERA2',copyOfObj[0][this.state.currentId+1].photo); 
+            //console.log('copyOfObj Z RENDERA2',copyOfObj[0][incrementedId].surname); 
         }
-        console.log(namesSurnames);
-  
 
 
-        console.log(this.props.route.time);
+        //console.log(this.props.route.time);
 
         return <div className="container">
-        {this.props.time}
             <div className="main-slider" id="mainSlider">
                 <button className="main-slider-prev" onClick={this.handleClickPrev}>
                     <span>Poprzedni slajd</span>
@@ -84,11 +116,11 @@ class Slider extends React.Component{
 
                 <div className="main-slider-slides-cnt">
                     <div className="main-slide active">
-                        <img className="main-slide-image" src="http://www.dailygossip.org/wp-content/uploads/2017/03/antonio-banderas.jpg" />
+                        <img className="main-slide-image" src={this.state.photo} />
                         <div className="main-slide-text">
-                            Antonio Banderas,
-                            Spanish<br/>
-                            IMDB: http://www.imdb.com/name/nm0000104/
+                            {this.state.name} {this.state.surname},
+                             {this.state.nationality}<br/>
+                            IMDB: {this.state.imdb}
                         </div>
                     </div>
                     <div className="main-slide">
